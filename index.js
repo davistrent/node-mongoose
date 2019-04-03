4428bd661bc42c5727f242581d55a694dd72dc2f
Davis G. Trent <davis@davistrent.com>
2019-04-03 15:57:00 -0700

    Mongoose Part 3.

--- index.js
+++ index.js
@@ -14,15 +14,33 @@ connect.then((db) => {
     .then(dish => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },
+        {
+            new: true
+        }).exec();
     })
-    .then(dishes => {
-        console.log(dishes);
+    .then(dish => {
+        console.log(dish);
 
-        return Dishes.remove({});
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio',
+        });
+
+        return dish.save();
+
+    })
+    .then(dish => {
+        console.log(dish);
+
+        return Dishes.remove();
     })
     .then((result) => {
         console.log("Removed all dishes: ", result);
+        
         return mongoose.connection.close();
     })
     .catch(err => {
